5f81eb63c2b4aa77b8213f1f15eb594831b47c34
Raman Shalupau <raman.shalupau@gmail.com>
2017-11-14 10:01:13 +0800

    some improvements and documentation

--- some-code.js
+++ some-code.js
@@ -2,9 +2,17 @@
  * Lovely comments are lovely. So here we are!
  * Since the task was to "To apply as a JS wiz/full stack guy or gal, give us some code (jobs@parity.io) that processes this into a lovely-looking web (HTML/CSS/JS) document"
  * Since the wording of the task allowed for it, i had to make a few compromises.
- * Not SEO friendly
+ * Not SEO friendly.
+ *
+ * Also, I dont always write comments and documentation,
+ * but when I to, that's typically to entertain new employers & clients ;)
+ * Also I went for vanillajs. Because using frameworks would be an overkill for this task.
  */
+
 const typeOf = (value) => {
+  // Because simple `typeof` does not detect arrays. Sadness.
+  // I called up Brendan Eich and complained about that, but he said he's too busy with Brave… 😒
+  // Whatever ¯\_(ツ)_/¯
   var s = typeof value;
   if (s === 'object') {
     if (value) {
@@ -18,7 +26,7 @@ const typeOf = (value) => {
   return s;
 }
 
-const iterate = (node, parent,  depth) => {
+const iterate = (node, parent, depth = 0) => {
   for (var key in node) {
     const datatype = typeOf(node[key])
     let div = document.createElement('div');
@@ -29,6 +37,11 @@ const iterate = (node, parent,  depth) => {
     div.appendChild(title);
     switch (datatype) {
       case 'number':
+        // Making sure date values look lovely. However real unixoids can convert EPOC timestamps in their sleep
+        if (/date/gi.test(key)) {
+          node[key] = new Date(node[key]).toDateString()
+        }
+        // Now that we formatted our date, let's default to jump to default behaviour 👇
       case 'string':
         const el = document.createElement('span')
         el.textContent = node[key]
@@ -50,11 +63,9 @@ const iterate = (node, parent,  depth) => {
       case 'object':
         iterate(node[key], div, depth+1);
         break;
-      default:
-        break;
 
     }
   }
 }
 
-iterate(job, document.getElementById('app'), 0)
+iterate(job, document.getElementById('app'))
